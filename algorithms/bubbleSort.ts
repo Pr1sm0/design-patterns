e8c56dc395486f12f88aee8c5e83ba08f71e5325
Vasyl Sava <Vasyard@gmail.com>
2021-02-04 13:24:00 +0200

    Change bubble sort implementation

--- algorithms/bubbleSort.ts
+++ algorithms/bubbleSort.ts
@@ -1,15 +1,18 @@
 function bubbleSort(array: number[]): number[] {
-  array = array.slice();
-  for(let i = 0; i < array.length; i++) {
-      for(let j = 0; j < array.length - 1; j++) {
-        if(array[j] > array[j + 1]) {
-            let swap = array[j];
-            array[j] = array[j + 1];
-            array[j + 1] = swap;
+  const arr = [...array];
+  let swap = false;
+  for(let i = 0; i < arr.length; i++) {
+      for(let j = 0; j < arr.length - 1; j++) {
+        if(arr[j] > arr[j + 1]) {
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+          swap = true;
         }
       }
+    if (!swap) {
+      return arr;
+    }
   }
-  return array;
+  return arr;
 }
 
 const result = bubbleSort([23,444,32,786,0,-99,12,54,753,43]);
